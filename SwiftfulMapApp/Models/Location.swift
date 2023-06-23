fa471aef3c7cfe652a152333a8d5d960d506d010
Artem <moiseev@physics.msu.ru>
2023-06-23 12:47:59 +0300

    Add assets, Location model

---
+++ SwiftfulMapApp/Models/Location.swift
@@ -0,0 +1,20 @@
+//
+//  Location.swift
+//  SwiftfulMapApp
+//
+//  Created by Artem on 23.06.2023.
+//
+
+import Foundation
+import MapKit
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+    
+}
+
+
